d1a9b16685b6e9b601892a654f258212e5ae834b
Felix Wolf <x3mdroid@gmail.com>
2023-02-16 19:50:59 +0100

    adapted script for server

--- volleyballScript.py
+++ volleyballScript.py
@@ -28,8 +28,8 @@ def getKursPostfix(day):
 
 ### configures the driver by getting the executable etc.
 def configureDriver():
-	THISPATH = os.path.dirname(os.path.abspath(__file__))
-	DRIVER_DIR = str(os.path.abspath(THISPATH + "/chromedriver"))
+	this_path = os.path.dirname(os.path.abspath(__file__))
+	driver_dir = str(os.path.abspath(this_path + "/chromedriver"))
 
 	#usr_input = "Mo/Spielkurs/Mi/Do/Sa/So: "
 	#while usr_input not in [ "Mo", "Mi","Do", "Sa", "So", "Spielkurs"]:
@@ -94,7 +94,7 @@ def bookCourse(driver):
 	#load credentials.json
 	email = ""
 	password = ""
-	with open('credentials.json') as json_file:
+	with open(os.path.dirname(os.path.abspath(__file__)) + '/credentials.json') as json_file:
 		data = json.load(json_file)
 		for person in data["users"]:
 			email = person["email"]
